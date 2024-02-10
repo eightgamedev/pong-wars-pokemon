a4f070dbce74208e03d01b29ad01cef6029a6cce
eight <hgrmtr8@gmail.com>
2024-02-11 00:39:19 +0900

    fix code style

--- include/SingleTypeWar.hpp
+++ include/SingleTypeWar.hpp
@@ -1,11 +1,33 @@
 ﻿# pragma once
 # include "Common.hpp"
 
-void updateBlockCountHistory(const Type type, const double time, const int32 countChange, Array<Array<std::pair<double, size_t>>>& blockCountsHistory);
-void updateBlock(Block& block, const Ball& ball, const double time, const int32 change, HashTable<Type, size_t>& blockCounts, Array<Array<std::pair<double, size_t>>>& blockCountsHistory);
-void checkCollisionAndBounce(Ball& ball, Grid<Block>& blocks, HashTable<Type, size_t>& blockCounts, Array<Array<std::pair<double, size_t>>>& blockCountsHistory, Size GridSize,
+void updateBlockCountHistory(
+	const Type type,
+	const double time,
+	const int32 countChange,
+	Array<Array<std::pair<double, size_t>>>& blockCountsHistory
+);
+void updateBlock(
+	Block& block,
+	const Ball& ball,
+	const double time,
+	const int32 change,
+	HashTable<Type, size_t>& blockCounts,
+	Array<Array<std::pair<double, size_t>>>& blockCountsHistory
+);
+void checkCollisionAndBounce(
+	Ball& ball,
+	Grid<Block>& blocks,
+	HashTable<Type, size_t>& blockCounts,
+	Array<Array<std::pair<double, size_t>>>& blockCountsHistory,
+	Size GridSize,
 	Vec2 BlockSize);
-void adjustBallCounts(Array<Ball>& balls, const Grid<Block>& blocks, HashTable<Type, size_t>& ballCounts, const HashTable<Type, size_t>& blockCounts);
+void adjustBallCounts(
+	Array<Ball>& balls,
+	const Grid<Block>& blocks,
+	HashTable<Type, size_t>& ballCounts,
+	const HashTable<Type, size_t>& blockCounts
+);
 void drawGraph(const Array<Array<std::pair<double, size_t>>>& blockCountsHistory);
 
 class SingleTypeWar : public App::Scene
