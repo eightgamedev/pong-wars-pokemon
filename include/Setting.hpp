ff2d2050f7787dd97b9102c8174b4ce75e0aa49f
eight <hgrmtr8@gmail.com>
2024-02-11 23:09:18 +0900

    add fullscreenbutton

--- include/Setting.hpp
+++ include/Setting.hpp
@@ -12,4 +12,5 @@ public:
 private:
 	CustomizedSimpleGUI::Button m_singleTypeWarButton{ U"Single Type War", 100, Vec2{ 100, 100 } };
 	CustomizedSimpleGUI::Button m_multipleTypeWarButton{ U"Multiple Type War", 100, Vec2{ 100, 300 } };
+	CustomizedSimpleGUI::Button m_switchScreenModeButton{ U"Switch Screen Mode", 30, Vec2{ 1500, 800 } };
 };
--- source/Setting.cpp
+++ source/Setting.cpp
@@ -9,6 +9,7 @@ void Setting::update()
 {
 	m_singleTypeWarButton.update();
 	m_multipleTypeWarButton.update();
+	m_switchScreenModeButton.update();
 	if (m_singleTypeWarButton.isPushed())
 	{
 		changeScene(State::SingleTypeWar);
@@ -17,10 +18,22 @@ void Setting::update()
 	{
 		changeScene(State::MultipleTypeWar);
 	}
+	if (m_switchScreenModeButton.isPushed())
+	{
+		if (Window::GetState().fullscreen)
+		{
+			Window::SetFullscreen(false);
+		}
+		else
+		{
+			Window::SetFullscreen(true);
+		}
+	}
 }
 
 void Setting::draw() const
 {
 	m_singleTypeWarButton.draw();
 	m_multipleTypeWarButton.draw();
+	m_switchScreenModeButton.draw();
 }
